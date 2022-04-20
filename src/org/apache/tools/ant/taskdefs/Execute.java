6018458629f3ff2d9e7ef69caa089326f55fa34c
Simi Lalwani <similalwani@Simis-MacBook-Air.local>
2022-04-20 00:30:31 -0400

    Issue Commit 5

--- src/org/apache/tools/ant/taskdefs/Execute.java
+++ src/org/apache/tools/ant/taskdefs/Execute.java
@@ -155,6 +155,7 @@ public class Execute {
      * @return a map containing the environment variables.
      * @since Ant 1.8.2
      */
+    //TODO: We have to get rid of the empty if() statement.
     public static synchronized Map/*<String,String>*/ getEnvironmentVariables() {
         if (procEnvironment != null) {
             return procEnvironment;
